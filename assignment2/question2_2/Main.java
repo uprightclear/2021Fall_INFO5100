58e845290cd797fe5acab2ba8ac209dc81c6f8ab
uprightclear <uprightclear@gmail.com>
2021-10-08 12:28:20 -0700

    assignment2 update

---
+++ assignment2/question2_2/Main.java
@@ -0,0 +1,12 @@
+package question2_2;
+
+public class Main {
+    public static void main(String[] args) {
+        Sum sum = new Sum();
+        System.out.println(sum.add(2, 3));
+        System.out.println(sum.add(4, 9, 12));
+        System.out.println(sum.add(4, 5.0));
+        System.out.println(sum.add(15.5, 5));
+        System.out.println(sum.add(1.0, 6.4));
+    }
+}
--- assignment2/question2_2/Sum.java
+++ assignment2/question2_2/Sum.java
@@ -3,4 +3,23 @@ package question2_2;
 public class Sum {
     public Sum() {}
 
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int add(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+
+    public double add(int num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double add(double num1, int num2) {
+        return num1 + num2;
+    }
+
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
 }
