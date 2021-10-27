91c58301447c26a1ca22b6bc33e8916b4e511fba
uprightclear <uprightclear@gmail.com>
2021-10-26 20:23:18 -0700

    midterm update

--- midterm/Q4/Main.java
+++ midterm/Q4/Main.java
@@ -9,19 +9,23 @@ public class Main {
         System.out.println(longestSubstring(example));
     }
 
-    public static int longestSubstring(String s) {
+    public static String longestSubstring(String s) {
         Set<Character> set = new HashSet<>();
         int len = s.length();
         int end = 0;
         int ans = 0;
+        String res = "";
         for(int i = 0; i < len; i++) {
             if(i > 0) set.remove(s.charAt(i - 1));
             while(end < len) {
                 if(set.add(s.charAt(end))) end++;
                 else break;
             }
+            if(end - i > ans) {
+                res = s.substring(i, end);
+            }
             ans = Math.max(ans, end - i);
         }
-        return ans;
+        return res;
     }
 }
